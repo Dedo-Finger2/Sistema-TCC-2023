651daa6ec746d13ea8682dbf221e2715bf64e930
WendsondaSilva <Wendson.ferreira@hotmail.com>
2023-10-10 20:52:41 -0300

    Aceita ae, e confere as modificações nas tabelas.

--- app/Models/Itinerario.php
+++ app/Models/Itinerario.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Itinerario extends Model
 {
     use HasFactory;
+
+    protected $timestamps = false;
+
+    public $fillable = [
+        'codigo_itinerario',
+        'id_onibus',
+        'id_rota',
+    ];
 }
--- app/Models/LocalRequisitado.php
+++ app/Models/LocalRequisitado.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class LocalRequisitado extends Model
 {
     use HasFactory;
+
+    protected $timestamps = false;
+    public $table = "locais_requisitados";
+
+    public $fillable = [
+        'nome',
+        'id_endereco',
+    ];
 }
