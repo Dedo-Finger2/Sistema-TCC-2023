d3b68ec7e69bfa42a0d883e8584c512adf146a37
Dedo_Finger2 <antonioimportant@gmail.com>
2023-10-10 21:10:42 -0300

    configuração modelos greg

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Usuario extends Model
 {
     use HasFactory;
+
+    protected $timestamps = false;
+
+    public $fillable = [
+        'id_endereco',
+        'nome',
+        'email',
+        'senha',
+    ];
+
+    public function endereco(): HasMany
+    {
+        return $this->hasMany(Endereco::class, 'id_endereco', 'id_usuario');
+    }
+
 }
