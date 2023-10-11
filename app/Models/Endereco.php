af8c57e6687867b81df6270dfc377c088bb01896
Joice Almeida <leone.almeida@ba.estudante.senai.br>
2023-10-10 21:16:47 -0300

    Leone models

--- app/Models/Endereco.php
+++ app/Models/Endereco.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Endereco extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        'logradouro',
+        'bairro',
+        'cidade',
+    ];
+    protected $timestamps = false;
 }
--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Feedback extends Model
 {
     use HasFactory;
+    // Configurando o model
+
+    /**
+     * lista com os nomes das colunas na migration
+     * OBS averiguar colunas na migration feedback
+     */
+    public $fillable = [
+        'comentario',
+        'data',
+        'feedback',
+        'id_usuario',
+    ];
+
+    /**
+     * caso a migration não tenha a coluna create created_at ou update_at
+     * coloque protected $timestamps = false;
+     */
+    protected $timestamps = false;
+
+    /**
+     * relacionamento um para muitos deve ser feiro com esse método
+     * @return hasMany
+     * OBS: na model deve ser colocado o método belongTo
+     *
+     */
+
+    public function usuarios(): HasMany
+    {
+        //
+        return $this->hasMany(Usuario::class,"id_usuario","id_feedback");
+    }
 }
