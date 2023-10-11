812763dfeb3a675411bdff0764c2ad3d029a8e9d
Antonio Mauricio <127690696+Dedo-Finger2@users.noreply.github.com>
2023-10-11 06:41:09 -0700

    parcial factories done

--- database/factories/EmpresaFactory.php
+++ database/factories/EmpresaFactory.php
@@ -17,7 +17,22 @@ class EmpresaFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            /*
+            | ----------------------------------------------
+            | Explicação
+            | ----------------------------------------------
+            | A estrutura pra usar as factories é essa:
+            | 'coluna' => $this->faker->método(parâmetro),
+            | ----------------------------------------------
+            | company() - Gera nome de empresas aleatórios
+            | companyEmail() - Gera emails de empresas aleatórios
+            | companySuffix() - Gera abreviações de nome de empresas aleatórios
+            | password() - Gera senhas aleatórias
+            */
+            'nome' => $this->faker->company(),
+            'email' => $this->faker->companyEmail(),
+            'cnpj' => $this->faker->companySuffix(),
+            'senha' => $this->faker->password()
         ];
     }
 }
--- database/factories/ItinerarioFactory.php
+++ database/factories/ItinerarioFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Onibus;
+use App\Models\Rota;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,33 @@ class ItinerarioFactory extends Factory
      */
     public function definition(): array
     {
+        /*
+        | ---------------------------------------------
+        | Explicação
+        | ---------------------------------------------
+        | Pegando todos os IDs que existem no nosso
+        | banco de dados e guardando dentro de uma variável (array)
+        | ---------------------------------------------
+        | Modelo::all() - Pega todas as linhas da tabela que o modelo representa
+        | pluck('id') - Pega apenas o ID de todos os dados da tabela
+        */
+        $idOnibus = Onibus::all()->pluck('id');
+        $idRotas = Rota::all()->pluck('id');
+
         return [
-            //
+            /*
+            | ----------------------------------------------
+            | Explicação
+            | ----------------------------------------------
+            | A estrutura pra usar as factories é essa:
+            | 'coluna' => $this->faker->método(parâmetro),
+            | ----------------------------------------------
+            | randomAscii() - Gera ??? aleatórios
+            | randomElement() - Pega um item aleatório de um array passado como parâmetro
+            */
+            'codigo_itinerario' => $this->faker->randomAscii(),
+            'id_onibus' => $this->faker->randomElement($idOnibus),
+            'id_rota' => $this->faker->randomElement($idRotas),
         ];
     }
 }
