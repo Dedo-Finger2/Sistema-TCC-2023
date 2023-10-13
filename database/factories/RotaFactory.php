66217fb13902ae2c41efbb2c73af53b9cc92e1ff
WendsondaSilva <Wendson.ferreira@hotmail.com>
2023-10-13 19:19:34 -0300

    Mudanças nas  "factory" (Rota e VoltaOnibus).

--- database/factories/RotaFactory.php
+++ database/factories/RotaFactory.php
@@ -2,13 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\Rota;
+use App\Models\VoltaOnibus;
+use App\Models\IdaOnibus;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rota>
- */
 class RotaFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Rota::class;
+
     /**
      * Define the model's default state.
      *
@@ -16,8 +23,42 @@ class RotaFactory extends Factory
      */
     public function definition(): array
     {
+        /*
+        | ----------------------------------------------
+        | Explicação
+        | ----------------------------------------------
+        | Obtendo todos os IDs das voltas de ônibus existentes no banco de dados
+        | e armazenando-os em uma variável (array).
+        | ----------------------------------------------
+        | VoltaOnibus::pluck('id') - Obtém apenas os IDs de todos os registros da tabela 'voltas_onibus'
+        | toArray() - Converte a coleção de IDs em um array
+        */
+        $idVoltasOnibus = VoltaOnibus::pluck('id')->toArray();
+
+        /*
+        | ----------------------------------------------
+        | Explicação
+        | ----------------------------------------------
+        | Obtendo todos os IDs das idas de ônibus existentes no banco de dados
+        | e armazenando-os em uma variável (array).
+        | ----------------------------------------------
+        | IdaOnibus::pluck('id') - Obtém apenas os IDs de todos os registros da tabela 'idas_onibus'
+        | toArray() - Converte a coleção de IDs em um array
+        */
+        $idIdasOnibus = IdaOnibus::pluck('id')->toArray();
+
         return [
-            //
+            /*
+            | ----------------------------------------------
+            | Explicação
+            | ----------------------------------------------
+            | A estrutura para usar as factories é a seguinte:
+            | 'coluna' => $this->faker->método(parâmetro),
+            | ----------------------------------------------
+            | randomElement() - Seleciona um elemento aleatório de um array passado como parâmetro
+            */
+            'id_volta_onibus' => $this->faker->randomElement($idVoltasOnibus),
+            'id_ida_onibus' => $this->faker->randomElement($idIdasOnibus),
         ];
     }
 }
--- database/factories/VoltaOnibusFactory.php
+++ database/factories/VoltaOnibusFactory.php
@@ -2,22 +2,51 @@
 
 namespace Database\Factories;
 
+use App\Models\VoltaOnibus;
+use App\Models\Endereco;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\VoltaOnibus>
- */
 class VoltaOnibusFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = VoltaOnibus::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
+        /*
+        | ----------------------------------------------
+        | Explicação
+        | ----------------------------------------------
+        | Obtendo todos os IDs dos endereços existentes no banco de dados
+        | e armazenando-os em uma variável (array).
+        | ----------------------------------------------
+        | Endereco::pluck('id') - Obtém apenas os IDs de todos os registros da tabela 'enderecos'
+        | toArray() - Converte a coleção de IDs em um array
+        */
+        $idEnderecos = Endereco::pluck('id')->toArray();
+
         return [
-            //
+            /*
+            | ----------------------------------------------
+            | Explicação
+            | ----------------------------------------------
+            | A estrutura para usar as factories é a seguinte:
+            | 'coluna' => $this->faker->método(parâmetro),
+            | ----------------------------------------------
+            | time() - Gera um valor de hora aleatório
+            | randomElement() - Seleciona um elemento aleatório de um array passado como parâmetro
+            */
+            'horario' => $this->faker->time(),
+            'id_endereco' => $this->faker->randomElement($idEnderecos),
         ];
     }
 }
