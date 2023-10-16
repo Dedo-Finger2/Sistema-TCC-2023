066a1eb900cb6aaa665f4bf6f2f88053251e1727
WendsondaSilva <Wendson.ferreira@hotmail.com>
2023-10-16 19:48:53 -0300

    Factory para as tabelas: Usuários e Onibus.

--- database/factories/OnibusFactory.php
+++ database/factories/OnibusFactory.php
@@ -2,22 +2,41 @@
 
 namespace Database\Factories;
 
+use App\Models\Onibus;
+use App\Models\Empresa;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Onibus>
- */
 class OnibusFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Onibus::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
+        $IdEmpresas = Empresa::all()->pluck('id');
+
         return [
-            //
+            /*
+            | ----------------------------------------------
+            | Explicação
+            | ----------------------------------------------
+            | A estrutura para usar as factories é a seguinte:
+            | 'coluna' => $this->faker->método(parâmetro),
+            | ----------------------------------------------
+            | numberBetween() - Gera um número aleatório entre os valores fornecidos
+            | randomElement() - Gera um único dígito aleatório
+            */
+            'numeracao' => $this->faker->numberBetween(1, 100),
+            'id_empresa' => $this->faker->randomElement($idEmpresas),
         ];
     }
 }
--- database/factories/UsuarioFactory.php
+++ database/factories/UsuarioFactory.php
@@ -2,22 +2,43 @@
 
 namespace Database\Factories;
 
+use App\Models\Usuario;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Usuario>
- */
 class UsuarioFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Usuario::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
+        $idEnderecos = Endereco::all()->pluck('id');
+
         return [
-            //
+            /*
+            | ----------------------------------------------
+            | Explicação
+            | ----------------------------------------------
+            | A estrutura para usar as factories é a seguinte:
+            | 'coluna' => $this->faker->método(parâmetro),
+            | ----------------------------------------------
+            | nome() - Gera um nome aleatório
+            | email() - Gera um endereço de e-mail válido
+            | senha() - Gera uma senha criptografada aleatória
+            */
+            'nome' => $this->faker->name(),
+            'email' => $this->faker->email(),
+            'senha' => $this->faker->password(),
+            'id_endereco' => $this->faker->randomElement($idEnderecos),
         ];
     }
 }
