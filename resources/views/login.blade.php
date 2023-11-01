8c295d52c40a48dd163ec93638d2bedfd4857f0b
WendsondaSilva <Wendson.ferreira@hotmail.com>
2023-11-01 20:01:00 -0300

    Atualização das telas de login e cadastro.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,4 +5,27 @@
 {{-- Sessão onde vai ser colocado todo o conteúdo do body desta página --}}
 @section('content')
 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tela de Login de Usuário</title>
+</head>
+<body>
+  <h1>Tela de Login de Usuário</h1>
+
+  <form>
+    <label for="email">Endereço de e-mail:</label>
+    <input type="email" id="email" name="email" required><br>
+
+    <label for="senha">Senha:</label>
+    <input type="password" id="senha" name="senha" required><br>
+
+    <input type="submit" value="Login">
+  </form>
+
+  <p>Esqueceu sua senha? <a href="recuperar-senha.html">Clique aqui</a>.</p>
+  <p>Não tem uma conta? <a href="cadastro.html">Cadastre-se aqui</a>.</p>
+</body>
+</html>
+
 @endsection
