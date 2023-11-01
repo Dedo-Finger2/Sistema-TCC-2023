722b929fc3a417a4006db42bd53d0c048b5428bc
Greg <antonioimportant@gmail.com>
2023-11-01 16:05:41 -0300

    footer and navbar component added

---
+++ resources/views/Componentes/footer.blade.php
@@ -0,0 +1,1 @@
+<footer>&copy; TCC - SENAI - 2023</footer>
---
+++ resources/views/Componentes/navbar.blade.php
@@ -0,0 +1,1 @@
+<a href="#">Login</a> - <a href="#">Cadastro</a> - <a href="#">Busca</a> - <a href="#">Rotas</a> - <a href="#">Feedback</a> - <a href="#">Painel de controle</a>
