c3170c0b397cfb1c2d1ced1969ca98aaeab4a493
Greg <antonioimportant@gmail.com>
2023-11-01 15:53:36 -0300

    basic layout aplicado

---
+++ resources/views/Empresa/painelControle.blade.php
@@ -0,0 +1,8 @@
+@extends('layout') {{-- Buscando o layout padrão e aplicando nesta página --}}
+
+@section('title', 'Painel de controle - ???') {{-- Inserindo o título desta página --}}
+
+{{-- Sessão onde vai ser colocado todo o conteúdo do body desta página --}}
+@section('content')
+
+@endsection
---
+++ resources/views/Usuario/rotas.blade.php
@@ -0,0 +1,8 @@
+@extends('layout') {{-- Buscando o layout padrão e aplicando nesta página --}}
+
+@section('title', 'Visualizando rotas - ???') {{-- Inserindo o título desta página --}}
+
+{{-- Sessão onde vai ser colocado todo o conteúdo do body desta página --}}
+@section('content')
+
+@endsection
