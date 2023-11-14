024b4956649acf6cc2486a734baf0ebf4227c16d
WendsondaSilva <Wendson.ferreira@hotmail.com>
2023-11-14 19:31:04 -0300

    Telas modificadas e nova pasta criada.

--- resources/views/Usuario/cadastro.blade.php
+++ resources/views/Usuario/cadastro.blade.php
@@ -5,80 +5,35 @@
 {{-- Sessão onde vai ser colocado todo o conteúdo do body desta página --}}
 @section('content')
 
-<!DOCTYPE html>
-<html lang="pt-br">
+    <h1>Tela de Cadastro de Usuário</h1>
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Tela de Cadastro de Usuário</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
+    {{-- Colocar mensagem à esquerda do formulário seguindo o layout no Figma --}}
 
-    <body>
-    <div class="container-fluid">
-        <div class="row">
-        <div class="col-md-6 bg-dark text-white d-flex flex-column justify-content-start align-items-start" style="height: 100vh;">
-            <div class="d-flex align-items-center">
-            <img src="images/Logo-TCC.jpeg" alt="Logo" style="width: 75px;" class="me-2 rounded-3 mt-2">
-            <h2 class="mt-3">Título ao lado do logo</h2>
-            </div>
-            <div class="content mt-5 text-left">
-            <h2 class="text-left">Bem-vindo</h2>
-            <p class="text-left">
-                <p>
-                Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo
-                Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo
-                Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo
-                Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo Frase de Bem-vindo
-                </p>
-            </div>
-            </div>
-            <div class="col-md-6 bg-light d-flex flex-column align-items-center justify-content-center" style="height: 100vh;">
-            <div class="login-container w-50">
-                <div class="container-title">
-                <h2 class="text-center">Cadastro de Usuário</h2>
-                </div>
-                <form action="#" method="POST" name="cadastro">
-                <div class="form-group">
-                    <label for="nome">Nome:</label>
-                    <input type="text" id="nome" name="nome" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="email">E-mail:</label>
-                    <input type="email" id="email" name="email" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="senha">Senha:</label>
-                    <input type="password" id="senha" name="senha" class="form-control">
-                </div>
-                <div class="form-group">
-                    <div class="row">
-                    <div class="col">
-                        <label for="logradouro">Logradouro:</label>
-                        <input type="text" id="logradouro" name="logradouro" class="form-control">
-                    </div>
-                    <div class="col">
-                        <label for="bairro">Bairro:</label>
-                        <input type="text" id="bairro" name="bairro" class="form-control">
-                    </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="cidade">Cidade:</label>
-                    <input type="text" id="cidade" name="cidade" class="form-control">
-                </div>
-                <br>
-                <input type="submit" value="Cadastrar" class="btn btn-primary w-100">
-                </form>
-                <p class="mt-3 text-center">Já tem uma conta? <a href="/login">Faça login aqui</a>.</p>
-            </div>
-            </div>
-        </div>
-        </div>
+    <form action="{{ route('registerUser') }}" method="POST" name="#">
+        @csrf
+        <label for="nome">Nome completo:</label>
+        <input type="text" id="nome" name="nome"><br>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-7W3Xu28E1o1RvLw3b8U2RkXoA5b9a6G86j5FqzSN2CCJ5SdUZ0gH9U1F5tLPMfFo" crossorigin="anonymous"></script>
-  </body>
-</html>
+        <label for="email">Endereço de e-mail:</label>
+        <input type="email" id="email" name="email"><br>
+
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha"><br>
+
+        {{-- Substituir esses selects pelos inputs do Select2 --}}
+        </select><br>
+
+        <label for="bairro">Bairro:</label>
+        <select name="id_endereco" id="bairro" class="select-single">
+            <option value="id_endereco">Bairro1</option>
+            <option value="id_endereco">Bairro2</option>
+            <option value="id_endereco">Bairro3</option>
+            <option value="id_endereco">Bairro4</option>
+        </select><br>
+
+        <input type="submit" value="Cadastrar">
+    </form>
+
+    <p>Já tem uma conta? <a href="/login">Faça login aqui</a>.</p>
 
 @endsection
