0bfb9f9e59490f48804bb3b5dacb7e389184b145
Greg <antonioimportant@gmail.com>
2023-11-29 08:18:40 -0800

    STYLING REVAMP

--- resources/views/User/create.blade.php
+++ resources/views/User/create.blade.php
@@ -1,38 +1,89 @@
-@extends('layout') {{-- Buscando o layout padrão e aplicando nesta página --}}
+<!--Arquivo Modificado-->
+
+@extends('layoutWithoutNavFooter') {{-- Buscando o layout padrão e aplicando nesta página --}}
 
 @section('title', 'Cadastro - Usuário') {{-- Inserindo o título desta página --}}
 
 {{-- Sessão onde vai ser colocado todo o conteúdo do body desta página --}}
 @section('content')
 
-    <h1>Tela de Cadastro de Usuário</h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 text-white d-flex flex-column justify-content-start align-items-start"
+                style="height: 100vh; background-image: url('https://th.bing.com/th/id/OIG.bgujAhQDkpF9Rb5rYlH0?w=1024&h=1024&rs=1&pid=ImgDetMain'); background-color: rgba(3, 19, 5, 0.9); background-blend-mode: multiply;">
+                <div class="d-flex align-items-center mt-5">
+                    <img src="https://raw.githubusercontent.com/Dedo-Finger2/Sistema-TCC-2023/main/resources/imgs/logo_tcc_nobg_white.png"
+                        class="shadow-lg" alt="Logo" style="width: 75px;" class="me-2 rounded-3 mt-2">
+                    <h2 class="mt-3">Busca de rotas e visualização de itinerários</h2>
+                </div>
+                <div class="content mt-5 text-left">
+                    <h2 class="text-left">Bem-vindo</h2>
+                    <p class="text-left">
+                    <p>
+                        bla bla bla.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6 bg-light d-flex flex-column align-items-center justify-content-center"
+                style="height: 100vh;">
+                <div class="login-container w-50">
+                    @if (session('error'))
+                        <div class="alert alert-danger mb-5">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <div class="text-center">
+                        <img src="https://raw.githubusercontent.com/Dedo-Finger2/Sistema-TCC-2023/main/resources/imgs/logo_tcc_nobg.png"
+                            class="img-fluid" style="width: 100px" alt="">
+                    </div>
+                    <div class="container-title">
+                        <h2 class="text-center">Cadastro de Usuário</h2>
+                    </div>
+                    <hr>
+                    <form action="{{ route('users.store') }}" method="POST" name="cadastro">
+                        @csrf
 
-    {{-- Colocar mensagem à esquerda do formulário seguindo o layout no Figma --}}
+                        <div class="form-group">
+                            <label for="nome">Nome:</label>
+                            <input type="text" id="nome" name="nome" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">E-mail:</label>
+                            <input type="email" id="email" name="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="senha">Senha:</label>
+                            <input type="password" id="senha" name="senha" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="bairro">Bairro:</label>
+                            <select name="address_id" id="destino" class="select-single form-select">
+                                @foreach ($addresses as $address)
+                                    <option value="{{ $address->id }}">{{ $address->bairro }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <br>
+                        <input type="submit" value="Cadastrar" class="btn btn-outline-success w-100">
+                    </form>
+                    <p class="mt-3 text-center">Já tem uma conta? <a class="link-success" href="{{ route('login') }}">Faça
+                            login aqui</a>.</p>
+                </div>
+            </div>
+        </div>
 
-    <form action="{{ route('users.store') }}" method="POST" name="#">
-        @csrf
-        <label for="nome">Nome completo:</label>
-        <input type="text" id="nome" name="nome"><br>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-7W3Xu28E1o1RvLw3b8U2RkXoA5b9a6G86j5FqzSN2CCJ5SdUZ0gH9U1F5tLPMfFo" crossorigin="anonymous">
+        </script>
 
-        <label for="email">Endereço de e-mail:</label>
-        <input type="email" id="email" name="email"><br>
-
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="password"><br>
-
-        {{-- Substituir esses selects pelos inputs do Select2 --}}
-        </select><br>
-
-        <label for="bairro">Bairro:</label>
-        <select name="address_id" id="bairro" class="select-single">
-            @foreach ($addresses as $address)
-                <option value="{{ $address->id }}">{{ $address->bairro }}</option>
-            @endforeach
-        </select><br>
-
-        <input type="submit" value="Cadastrar">
-    </form>
-
-    <p>Já tem uma conta? <a href="{{ route('login') }}">Faça login aqui</a>.</p>
-
-@endsection
+    @endsection
--- resources/views/User/routes.blade.php
+++ resources/views/User/routes.blade.php
@@ -6,7 +6,6 @@
 @section('content')
 
     <main class="mb-5">
-
         <section class="py-2 text-center container">
             <div class="row py-lg-5">
                 <div class="col-lg-6 col-md-8 mx-auto">
@@ -20,15 +19,27 @@
             </div>
             <hr>
         </section>
+
         @if (session('warn'))
             <div class="alert alert-warning container w-25 text-center" role="alert">
                 <div>
                     <h2 class="text-center alert-heading">Aviso!</h2>
                 </div>
                 <p>{{ session('warn') }}</p>
-                <a href="{{ route('feedback.create') }}">Quero dar meu feedback!</a>
+                <a href="{{ route('feedback.create') }}">Quero dar meu feedback.</a>
             </div>
         @endif
+
+        @if (session('success'))
+            <div class="alert alert-success container w-25 text-center" role="alert">
+                <div>
+                    <h2 class="text-center alert-heading">Sucesso!</h2>
+                </div>
+                <p>{{ session('success') }}</p>
+                <a href="{{ route('feedback.create') }}">Quero dar meu feedback.</a>
+            </div>
+        @endif
+
         <div class="album py-5 bg-body-tertiary">
             <div class="container">
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-3">
@@ -53,16 +64,15 @@
                             </div>
                         @endforeach
                     @else
-                        <div class="container">
-                            <p class="fs-2 text-center">Nenhuma rota encontrada. Busque uma clicando neste <a
-                                    href="{{ route('routes.showSearchForm') }}">link</a></p>
+                        <div class="container mb-5 mt-5">
+                            <p class="fs-2 text-center">Nenhuma rota encontrada. Busque uma clicando neste <a href="{{ route('routes.showSearchForm') }}">link</a></p>
                         </div>
                     @endif
                 </div>
+                <div class="text-white mb-5">.</div>
+                <div class="text-white mb-5">.</div>
                 {{-- {{ $rotasEncontradas->links('pagination::bootstrap-4') }} --}}
             </div>
         </div>
     </main>
-
-    <a href="{{ route('feedback.create') }}">Quero dar meu feedback.</a>
 @endsection
